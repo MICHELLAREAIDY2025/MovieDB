a688c2a1738e2c584e1d26d98ee3ba0d1e5e3f9f
Michella <michella.reaidy@gmail.com>
2024-12-22 20:55:00 +0200

    express-API

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ app.get('*', (req, res) => {
   res.send('ok');
 });
 
+// Route for /test
+app.get('/test', (req, res) => {
+    res.json({ status: 200, message: "ok" });
+  });
+  
+  // Route for /time
+  app.get('/time', (req, res) => {
+    const currentTime = new Date();
+    const hours = String(currentTime.getHours()).padStart(2, '0');
+    const minutes = String(currentTime.getMinutes()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}`;
+    res.json({ status: 200, message: timeString });
+  });
+
 // Make the server listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
